09ee4a42425d1edc36f32f320f3882a1e9d98a88
Anastasia Ageeva <anastasia.o.ageeva@gmail.com>
2022-08-04 10:50:39 +0300

    Task 027 Sum Digits

---
+++ Task027_SumDigits/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int num = Int32.Parse(Console.ReadLine());
+int count = NumberOfDigits(num);
+int res = SumOfDigits(num, count);
+Console.WriteLine($"Сумма цифр числа {num} равна {res}");
+
+
+//дальше методы, вызов которых выше ^
+int NumberOfDigits(int number)
+{
+    int a = num;
+    int i = 0;
+    while (a > 0)
+    {
+        a = a / 10;
+        i++;
+    }
+    return i;
+}
+
+
+int SumOfDigits (int num1, int count1)
+{
+    int summ = 0;
+    int a = num1;
+    int b = num1;
+    for (int i = 1; i <= count; i++)
+    {
+        a = a % 10;
+        summ = summ + a;
+        a = b / 10;
+        b = a;
+    }
+    return summ;
+}
+
